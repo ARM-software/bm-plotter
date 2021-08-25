833291433d92e8a8a179c81df3455291842f6bf9
Jacob Bramley <jacob.bramley@arm.com>
2021-08-25 20:10:45 +0100

    Update the README to point to the new repository.

--- run-all-examples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copyright (c) 2016, ARM Limited and Contributors.
-# All rights reserved.
-# SPDX-Licence-Identifier: BSD-3-Clause
-
-set -eu
-
-parallel --verbose ::: examples/*.sh
